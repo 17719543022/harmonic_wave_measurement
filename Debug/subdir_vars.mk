ad27f2b4f00d67c9444aef31040a105a17758f7b
刘勇 <xin_nian13@163.com>
2022-05-09 10:29:27 +0800

    initial commit

---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../msp432e401y.cmd 
+
+C_SRCS += \
+../cmsis_msp432e4_dsp_example.c \
+../system_msp432e401y.c \
+../uartstdio.c 
+
+C_DEPS += \
+./cmsis_msp432e4_dsp_example.d \
+./system_msp432e401y.d \
+./uartstdio.d 
+
+OBJS += \
+./cmsis_msp432e4_dsp_example.obj \
+./system_msp432e401y.obj \
+./uartstdio.obj 
+
+OBJS__QUOTED += \
+"cmsis_msp432e4_dsp_example.obj" \
+"system_msp432e401y.obj" \
+"uartstdio.obj" 
+
+C_DEPS__QUOTED += \
+"cmsis_msp432e4_dsp_example.d" \
+"system_msp432e401y.d" \
+"uartstdio.d" 
+
+C_SRCS__QUOTED += \
+"../cmsis_msp432e4_dsp_example.c" \
+"../system_msp432e401y.c" \
+"../uartstdio.c" 
+
+
